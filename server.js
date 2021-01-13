af28c1c66ccfe94abecf92db28ec3eab2a607894
JohnDJake <john.d.jake@gmail.com>
2021-01-12 18:32:30 -0800

    set up basic express server with get access to html and assets

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import express, fs and path
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// Configure express server
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up HTML GET routes
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get("/assets/*", (req, res) => res.sendFile(path.join(__dirname, "public/assets", req.params["0"])));
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+// Start the server
+app.listen(PORT, err => {
+    if (err) {
+        console.error(err);
+    }
+    console.log(`App listening on port ${PORT}`);
+});
